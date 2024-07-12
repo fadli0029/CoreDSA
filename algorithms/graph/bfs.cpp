b4cc7dc6786055b52b13abfd8b8c1d2ec9809cc2
fadli0029 <mfarsani@ucsd.edu>
2024-07-12 11:29:57 -0700

    [ADD] bfs

--- algorithms/graph/bfs.cpp
+++ algorithms/graph/bfs.cpp
@@ -1,6 +1,6 @@
 /* ==============================================================================
-   This is an implementation of the Depth First Search (DFS) algorithm using a
-   stack explicitly. The implementation assumes the starting node to be `0` by
+   This is an implementation of the Breadth First Search (DFS) algorithm.
+   The implementation assumes the starting node to be `0` by
    default. It then prints the visited nodes in the order they are visited.
 
    @param n: the number of vertices in the graph
@@ -16,24 +16,25 @@
 
 #include "bits/stdc++.h"
 
-void dfs(int n, std::vector<std::vector<int>>& adj_mat) {
+void bfs(int n, std::vector<std::vector<int>>& adj_mat) {
   std::vector<int> record;
 
   // ====================================================
-  std::stack<int> st;
-  st.push(0);
+  std::queue<int> q;
   std::vector<bool> visited(n, false);
 
-  while (!st.empty()) {
-    int node = st.top();
-    st.pop();
-    if (!visited[node]) {
-      record.push_back(node);
-      visited[node] = true;
-      for (int i=0; i<n; i++) {
-        if (adj_mat[node][i] && !visited[i]) {
-          st.push(i);
-        }
+  q.push(0);
+  visited[0] = true;
+
+  while (!q.empty()) {
+    int node = q.front();
+    q.pop();
+    record.push_back(node);
+
+    for (int i = 0; i < n; i++) {
+      if (adj_mat[node][i] && !visited[i]) {
+        q.push(i);
+        visited[i] = true;
       }
     }
   }
@@ -51,24 +52,25 @@ void dfs(int n, std::vector<std::vector<int>>& adj_mat) {
   std::cout << "]" << std::endl;
 }
 
-void dfs(int n, std::vector<int> adj_list[]) {
+void bfs(int n, std::vector<int> adj_list[]) {
   std::vector<int> record;
 
   // ====================================================
-  std::stack<int> st;
-  st.push(0);
+  std::queue<int> q;
   std::vector<bool> visited(n, false);
 
-  while (!st.empty()) {
-    int node = st.top();
-    st.pop();
-    if (!visited[node]) {
-      record.push_back(node);
-      visited[node] = true;
-      for (auto const& neighbor : adj_list[node]) {
-        if (!visited[neighbor]) {
-          st.push(neighbor);
-        }
+  q.push(0);
+  visited[0] = true;
+
+  while (!q.empty()) {
+    int node = q.front();
+    q.pop();
+    record.push_back(node);
+
+    for (auto const& neighbor : adj_list[node]) {
+      if (!visited[neighbor]) {
+        q.push(neighbor);
+        visited[neighbor] = true;
       }
     }
   }
@@ -86,7 +88,7 @@ void dfs(int n, std::vector<int> adj_list[]) {
   std::cout << "]" << std::endl;
 }
 
-TEST_CASE("DFS with adjacency matrix", "[dfs][adjacency_matrix]") {
+TEST_CASE("BFS with adjacency matrix", "[bfs][adjacency_matrix]") {
   int n = 4;
   std::vector<std::vector<int>> adj_mat = {
       {0, 1, 0, 1},
@@ -94,42 +96,42 @@ TEST_CASE("DFS with adjacency matrix", "[dfs][adjacency_matrix]") {
       {0, 1, 0, 1},
       {1, 0, 1, 0}
   };
-
+  
   // Capture the output
   std::ostringstream oss;
   std::streambuf* p_cout_streambuf = std::cout.rdbuf();
   std::cout.rdbuf(oss.rdbuf());
 
-  dfs(n, adj_mat);
+  bfs(n, adj_mat);
 
   // Restore the original stream buffer
   std::cout.rdbuf(p_cout_streambuf);
 
-  REQUIRE(oss.str() == "[0, 3, 2, 1]\n");
+  REQUIRE(oss.str() == "[0, 1, 3, 2]\n");
 }
 
-TEST_CASE("DFS with adjacency list", "[dfs][adjacency_list]") {
+TEST_CASE("BFS with adjacency list", "[bfs][adjacency_list]") {
   int n = 4;
   std::vector<int> adj_list[4];
   adj_list[0] = {1, 3};
   adj_list[1] = {0, 2};
   adj_list[2] = {1, 3};
   adj_list[3] = {0, 2};
-
+  
   // Capture the output
   std::ostringstream oss;
   std::streambuf* p_cout_streambuf = std::cout.rdbuf();
   std::cout.rdbuf(oss.rdbuf());
 
-  dfs(n, adj_list);
+  bfs(n, adj_list);
 
   // Restore the original stream buffer
   std::cout.rdbuf(p_cout_streambuf);
 
-  REQUIRE(oss.str() == "[0, 3, 2, 1]\n");
+  REQUIRE(oss.str() == "[0, 1, 3, 2]\n");
 }
 
-TEST_CASE("DFS with disconnected graph - adjacency matrix", "[dfs][adjacency_matrix]") {
+TEST_CASE("BFS with disconnected graph - adjacency matrix", "[bfs][adjacency_matrix]") {
   int n = 5;
   std::vector<std::vector<int>> adj_mat = {
       {0, 1, 0, 0, 0},
@@ -138,13 +140,13 @@ TEST_CASE("DFS with disconnected graph - adjacency matrix", "[dfs][adjacency_mat
       {0, 0, 0, 0, 1},
       {0, 0, 0, 1, 0}
   };
-
+  
   // Capture the output
   std::ostringstream oss;
   std::streambuf* p_cout_streambuf = std::cout.rdbuf();
   std::cout.rdbuf(oss.rdbuf());
 
-  dfs(n, adj_mat);
+  bfs(n, adj_mat);
 
   // Restore the original stream buffer
   std::cout.rdbuf(p_cout_streambuf);
@@ -152,7 +154,7 @@ TEST_CASE("DFS with disconnected graph - adjacency matrix", "[dfs][adjacency_mat
   REQUIRE(oss.str() == "[0, 1, 2]\n");
 }
 
-TEST_CASE("DFS with disconnected graph - adjacency list", "[dfs][adjacency_list]") {
+TEST_CASE("BFS with disconnected graph - adjacency list", "[bfs][adjacency_list]") {
   int n = 5;
   std::vector<int> adj_list[5];
   adj_list[0] = {1};
@@ -160,13 +162,13 @@ TEST_CASE("DFS with disconnected graph - adjacency list", "[dfs][adjacency_list]
   adj_list[2] = {1};
   adj_list[3] = {4};
   adj_list[4] = {3};
-
+  
   // Capture the output
   std::ostringstream oss;
   std::streambuf* p_cout_streambuf = std::cout.rdbuf();
   std::cout.rdbuf(oss.rdbuf());
 
-  dfs(n, adj_list);
+  bfs(n, adj_list);
 
   // Restore the original stream buffer
   std::cout.rdbuf(p_cout_streambuf);
@@ -174,16 +176,16 @@ TEST_CASE("DFS with disconnected graph - adjacency list", "[dfs][adjacency_list]
   REQUIRE(oss.str() == "[0, 1, 2]\n");
 }
 
-TEST_CASE("DFS with single node graph - adjacency matrix", "[dfs][adjacency_matrix]") {
+TEST_CASE("BFS with single node graph - adjacency matrix", "[bfs][adjacency_matrix]") {
   int n = 1;
   std::vector<std::vector<int>> adj_mat = {{0}};
-
+  
   // Capture the output
   std::ostringstream oss;
   std::streambuf* p_cout_streambuf = std::cout.rdbuf();
   std::cout.rdbuf(oss.rdbuf());
 
-  dfs(n, adj_mat);
+  bfs(n, adj_mat);
 
   // Restore the original stream buffer
   std::cout.rdbuf(p_cout_streambuf);
@@ -191,16 +193,16 @@ TEST_CASE("DFS with single node graph - adjacency matrix", "[dfs][adjacency_matr
   REQUIRE(oss.str() == "[0]\n");
 }
 
-TEST_CASE("DFS with single node graph - adjacency list", "[dfs][adjacency_list]") {
+TEST_CASE("BFS with single node graph - adjacency list", "[bfs][adjacency_list]") {
   int n = 1;
   std::vector<int> adj_list[1];
-
+  
   // Capture the output
   std::ostringstream oss;
   std::streambuf* p_cout_streambuf = std::cout.rdbuf();
   std::cout.rdbuf(oss.rdbuf());
 
-  dfs(n, adj_list);
+  bfs(n, adj_list);
 
   // Restore the original stream buffer
   std::cout.rdbuf(p_cout_streambuf);
