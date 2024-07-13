3e4305108727e789222b9fa04b5c57996bbb0b3c
fadli0029 <mfarsani@ucsd.edu>
2024-07-13 08:53:50 -0700

    [ADD] hierholzer algo draft

--- algorithms/graph/hierholzer.cpp
+++ algorithms/graph/hierholzer.cpp
@@ -1,2 +1,10 @@
 // Example problem: 332. Reconstruct Itinerary
-// Explanation: https://algorithms.discrete.ma.tum.de/graph-algorithms/hierholzer/index_en.html
+// Pre-requisite:
+// - https://youtu.be/ycRuO-u6rt8?si=FZpFILldtZAbIcPa
+// - https://youtu.be/xR4sGgwtR2I?si=P6egT4GZW7jirs63
+// Explanation:
+// - https://algorithms.discrete.ma.tum.de/graph-algorithms/hierholzer/index_en.html
+// - https://youtu.be/8MpoO2zA2l4?si=8ra9eW4Y6fJDF9X9
+// - https://youtu.be/qZrfK2iE4UA?si=CDnE9pLz0XWKLi7A
+// - https://youtu.be/QQ3jO1dKjYQ?si=MhK80Ufog_xnpD4a
+
